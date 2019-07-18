7a1014608995b12d907d3ee1dbc2795a9c833441
helloworld2011 <1286243627@qq.com>
2019-07-18 16:48:09 +0800

    first commit

---
+++ kafka-1.0.0-src/kafka-1.0.0-src/examples/src/main/java/kafka/examples/Test.java
@@ -0,0 +1,13 @@
+package kafka.examples;
+
+/**
+ *@desc: 
+ *@author gxy
+ *@createDate 2019-07-04
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
